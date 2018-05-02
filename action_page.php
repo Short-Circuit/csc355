8a81521419af443ce2ca7651d2508d03b749a5d8
ShortCircuit908 <ShortCircuit908@gmail.com>
2018-05-02 14:19:59 -0700

    Make dummy action_page.php

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Caleb Milligan
+ * Created 5/2/2018
+ */
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Create an Album</title>
+	</head>
+	<body>
+		<h1>Not yet implemented</h1>
+	</body>
+</html>
